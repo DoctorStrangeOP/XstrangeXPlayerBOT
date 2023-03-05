a8097d1a565d6ed0a242ff45203ec459048a2bcd
XstrangeX <120216933+DoctorStrangeOP@users.noreply.github.com>
2023-03-05 16:29:33 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/DoctorStrangeOP/XstrangeXPlayerBOT /XstrangeXPlayerBOT
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/DoctorStrangeOP/XstrangeXPlayerBOT -b $BRANCH /XstrangeXPlayerBOT
 fi
-cd /VCPlayerBot
+cd /XstrangeXPlayerBOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
